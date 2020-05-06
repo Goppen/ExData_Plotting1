5f76e65b580a227b30c00d82ba66ea1d17ed2131
Goppen <santiago.oppen@gmail.com>
2020-05-06 20:17:53 -0300

    Changes

--- plot1.R
+++ plot1.R
@@ -7,4 +7,11 @@ tblconsumption$Date <- as.Date(strptime(tblconsumption$Date, format = "%d/%m/%Y"
 
 daysofinterest <- subset(tblconsumption, Date >= "2007-02-01" & Date <="2007-02-02" )
 
-daysofinterest$Global_active_power == "?")
+
+daysofinterest$Global_active_power <- as.numeric(as.character(daysofinterest$Global_active_power))
+
+png("plot1.png")
+
+hist(daysofinterest$Global_active_power, breaks = 20, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+
+dev.off()
--- plot3.R
+++ plot3.R
@@ -10,10 +10,11 @@ daysofinterest$DateTime <- strptime(paste(as.character(daysofinterest$Date), as.
 
 
 
-png("plot3.png")
+png("plot4.png")
 
 plot(daysofinterest$DateTime, as.numeric(as.character(daysofinterest$Sub_metering_1)), type = "l", xlab = "", ylab = "Energy sub metering")
 lines(daysofinterest$DateTime, as.numeric(as.character(daysofinterest$Sub_metering_2)), col = "red")
 lines(daysofinterest$DateTime, daysofinterest$Sub_metering_3, col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1)
 dev.off()
+
